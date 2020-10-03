fc6183a06532bff584b16e12c6d06553fd3dd76b
YUYANG MA <2452827701@qq.com>
2020-10-03 18:29:53 -0400

    initial commit

---
+++ Assets/_Scripts/Switch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Switch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void SwitchToGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
